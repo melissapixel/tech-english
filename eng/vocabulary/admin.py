b628e87711291222167cea87e52996bfd12ddd78
Mel <melissapixell@gmail.com>
2025-12-13 22:12:02 +0300

    reg models of vocabulary

--- eng/vocabulary/admin.py
+++ eng/vocabulary/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Word, Phrase, TextExample
 
 # Register your models here.
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    list_display = ['word_en', 'translation_ru', 'created_at']
+    search_fields = ['word_en', 'translation_ru']
+
+@admin.register(Phrase)
+class PhraseAdmin(admin.ModelAdmin):
+    list_display = ['word', 'example_en', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['example_en', 'translation_ru']
+
+@admin.register(TextExample)
+class TextExampleAdmin(admin.ModelAdmin):
+    list_display = ['word', 'example_en', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['example_en', 'translation_ru']
